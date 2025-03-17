512553cedb41ba4bed7cb682e69d52e952e1cb4e
Nile Kelly <nilekell@gmail.com>
2025-03-17 17:55:18 +0000

    refactoring into functions + .env support

--- scraper.py
+++ scraper.py
@@ -1,51 +1,104 @@
 import requests
 import sys
 import datetime as dt
+import smtplib
+import os
+from dotenv import load_dotenv
+from email.message import EmailMessage
 
-# PASSPORT_APPLICATION_WEB_URL = "https://jhcukconsular.youcanbook.me/"
-# CITIZENSHIP_APPLICATION_WEB_URL = "https://jhcukconsular-3.youcanbook.me/"
+# visit url > intent created > intent used to fetch availability key > availability key used to fetch availabilities
+# intents created for every browser session, find way to automate retrieval of new intent value
+# startSearchAt should be incremented by a month when slots_data is empty, use iteration
+
+# Jamaica passport application appointment web link: https://jhcukconsular.youcanbook.me/
+# Jamaica citizenship application appointment web link: https://jhcukconsular-3.youcanbook.me/
 
 AVAILABILITY_URL = "https://api.youcanbook.me/v1/availabilities"
 INTENTS_URL = "https://api.youcanbook.me/v1/intents"
-current_date = dt.datetime.today().strftime('%Y-%m-%d')
-search_date = "2025-06-11"
 
-PASSPORT_INTENTS_URL = f"{INTENTS_URL}/itt_8fc262a2-50fc-493c-a82b-cb0e2e41763b/availabilitykey?startSearchAt={search_date}"
-CITIZENSHIP_INTENTS_URL = f"{INTENTS_URL}/itt_8fc262a2-50fc-493c-a82b-cb0e2e41763b/availabilitykey?startSearchAt={search_date}"
+def get_intent_urls():
+    # current_date = dt.datetime.today().strftime('%Y-%m-%d')
+    # current_date = "2025-06-11" # date from available appointments
+    current_date = "2025-06-01"
+    # https://api.youcanbook.me/v1/intents/itt_00d55e5a-bcb7-418f-983b-970d70c980e8/availabilitykey?startSearchAt=2025-06-14
+    passport_url = f"{INTENTS_URL}/itt_00d55e5a-bcb7-418f-983b-970d70c980e8/availabilitykey?startSearchAt={current_date}"
+    citizenship_url = f"{INTENTS_URL}/itt_8fc262a2-50fc-493c-a82b-cb0e2e41763b/availabilitykey?startSearchAt={current_date}"
+    return passport_url, citizenship_url
 
-url = PASSPORT_INTENTS_URL
-response = requests.get(url)   
+def get_env():
+    load_dotenv()
+    email = os.getenv('EMAIL') # sender
+    password = os.getenv('PASSWORD') # google app-specific passwords can be created at https://myaccount.google.com/apppasswords
+    destination_email = os.getenv('DESTINATION_EMAIL')
+    return email, password, destination_email
 
-if response.status_code != 200:
-    print(f"Failed to fetch data from {url} - ERROR: {response.status_code}")
-    sys.exit(1)
+def fetch_availability_key(intents_url):
+    print("intents url:", intents_url)
 
-data = response.json()
-availability_key = data['key'] # availability keys expire after they are used to fetch data
-# print(availability_key)
+    response = requests.get(intents_url)   
 
-dates_url = f"{AVAILABILITY_URL}/{availability_key}"
-print(dates_url)
-dates_response = requests.get(dates_url)
+    if not response.ok:
+        print(f"Failed to fetch data from {intents_url} - ERROR: {response.status_code}, {response.content}")
+        sys.exit(1)
 
-if dates_response.status_code != 200:
-    print(f"Failed to fetch data from {dates_url} - ERROR: {dates_response.status_code}")
-    sys.exit(1)
-
-dates_data = dates_response.json()
-# print(dates_data)
-
-slots = dates_data['slots']
-if slots == []:
-    print(f"No available slots from {dates_url} - ERROR: {dates_response.status_code}")
-    sys.exit(0)
-
-for slot in slots:
-    num_free_slots = slot['freeUnits']
-    apt_timestamp_ms = slot['startsAt']
-    appointment_timestamp = int(apt_timestamp_ms) / 1000
-
-    appointment_datetime = dt.datetime.fromtimestamp(appointment_timestamp).strftime('%Y-%m-%d %H:%M:%S')
-    print(f"{num_free_slots} slots available at {appointment_datetime}")
+    data = response.json()
+    key = data['key'] # availability keys expire after they are used to fetch data
+    print("availability key:", key)
+    return key
 
 
+def fetch_available_slots(availability_key):
+    slots_url = f"{AVAILABILITY_URL}/{availability_key}"
+    print("slots url:", slots_url)
+    dates_response = requests.get(slots_url)
+    print("dates_response:", dates_response.status_code)
+
+    if not dates_response.ok:
+        print(f"Failed to fetch data from {slots_url} - ERROR: {dates_response.status_code}, {dates_response.content}")
+        sys.exit(1)
+
+    data = dates_response.json()
+    slots_data = data['slots']
+    print("slots_data:", slots_data)
+
+    if slots_data == []:
+        print(f"No available slots from {slots_url} - ERROR: {dates_response.status_code}")
+        sys.exit(0)
+
+    return slots_data
+
+def extract_readable_data(slots_data):
+    text = ""
+    for slot in slots_data:
+        num_free_slots = slot['freeUnits']
+        apt_timestamp_ms = slot['startsAt']
+        appointment_timestamp = int(apt_timestamp_ms) / 1000
+
+        appointment_datetime = dt.datetime.fromtimestamp(appointment_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+        text += f"{num_free_slots} slots available at {appointment_datetime}\n"
+
+        print("text:", text)
+
+    return text
+
+def send_email(subject, text):
+    smtp_server = smtplib.SMTP('smtp.gmail.com', 587) # creates SMTP session
+    smtp_server.starttls()
+    smtp_server.login(EMAIL, PASSWORD)
+    # construct message to be sent
+    message = EmailMessage()
+    message['Subject'] = subject
+    message['From'] = EMAIL
+    message['To'] = DESTINATION_EMAIL
+    message.set_content(text)
+    smtp_server.send_message(message)
+    # terminating the session
+    smtp_server.quit()
+
+
+PASSPORT_INTENTS_URL, CITIZENSHIP_INTENTS_URL = get_intent_urls()
+EMAIL, PASSWORD, DESTINATION_EMAIL = get_env()
+passport_availability_key = fetch_availability_key(PASSPORT_INTENTS_URL)
+passport_slots = fetch_available_slots(passport_availability_key)
+passport_text = extract_readable_data(passport_slots)
+# send_email("Available Appointments - Jamaican High Commission", passport_text)
